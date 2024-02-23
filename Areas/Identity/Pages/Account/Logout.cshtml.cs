e32897d904b667b1b02a1868e21b8dde1307db22
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-02-23 18:41:37 +0200

    Update Logout.cshtml.cs

--- Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -2,15 +2,11 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
-using System;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 
-namespace CourseNet.Areas.Identity.Pages.Account
+namespace CourseNet.Web.Areas.Identity.Pages.Account
 {
     public class LogoutModel : PageModel
     {
