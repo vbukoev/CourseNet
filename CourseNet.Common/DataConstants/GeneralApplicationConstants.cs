6523d3901163c5714b50252464906cbf40466513
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-03-10 23:03:16 +0200

    editited the default enities per page constant from 2 to 3 and removed studentId from selection of courses because it was unnecessary

--- CourseNet.Common/DataConstants/GeneralApplicationConstants.cs
+++ CourseNet.Common/DataConstants/GeneralApplicationConstants.cs
@@ -3,6 +3,6 @@
     public static class GeneralApplicationConstants
     {
         public const int DefaultPage = 1;
-        public const int EntitiesPerPage = 2;
+        public const int EntitiesPerPage = 3;
     }
 }
