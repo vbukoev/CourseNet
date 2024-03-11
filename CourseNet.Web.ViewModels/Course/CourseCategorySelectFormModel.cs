0187309dd09b218ac6c189599b377ee84e3b7384
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-03-11 23:31:40 +0200

    listing of all categories

---
+++ CourseNet.Web.ViewModels/Course/CourseCategorySelectFormModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseNet.Web.ViewModels.Course
+{
+    internal class CourseCategorySelectFormModel
+    {
+    }
+}
