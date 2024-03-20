8ab6735bcab4d61b8f394f3891951840eb9847ed
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-03-20 09:39:49 +0200

    Update LectureSelectionFormViewModel.cs

--- CourseNet.Web.ViewModels/Lecture/LectureSelectionFormViewModel.cs
+++ CourseNet.Web.ViewModels/Lecture/LectureSelectionFormViewModel.cs
@@ -1,5 +1,8 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
 using static CourseNet.Common.DataConstants.Lecture;
+
 namespace CourseNet.Web.ViewModels.Lecture
 {
     public class LectureSelectionFormViewModel
@@ -17,6 +20,10 @@ namespace CourseNet.Web.ViewModels.Lecture
         [Required]
         [Display(Name = "Дата")]
         public string Date { get; set; }
+
+        [Required]
+        public Guid CourseId { get; set; }
+
         
         public IEnumerable<LectureSelectionFormViewModel> Lectures { get; set; } = new List<LectureSelectionFormViewModel>();
     }
