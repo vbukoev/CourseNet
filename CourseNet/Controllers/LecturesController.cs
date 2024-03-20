1319a63bd4ea15367a38dc99ca1892a2d6f74967
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-03-20 09:39:55 +0200

    Update LecturesController.cs

--- CourseNet/Controllers/LecturesController.cs
+++ CourseNet/Controllers/LecturesController.cs
@@ -36,20 +36,12 @@ namespace CourseNet.Web.Controllers
         [HttpGet]
         public async Task<IActionResult> Create(string courseId)
         {
-            bool isInstructor = await instructorService.InstructorExistsByUserId(User.GetId());
-
-            if (!isInstructor)
-            {
-                TempData[ErrorMessage] = "Вие не сте инструктор! Трябва първо да станете инструктор, за да успеете да създадете лекция към курс";
-
-                return RedirectToAction("Become", "Instructor");
-            }
-
             try
             {
-                LectureSelectionFormViewModel viewModel = new LectureSelectionFormViewModel()
+                LectureSelectionFormViewModel viewModel = new LectureSelectionFormViewModel
                 {
-                    Lectures = await lecturesService.GetAllLecturesForCourseAsync(courseId)
+                    Lectures = await lecturesService.GetAllLecturesForCourseAsync(courseId),
+                    CourseId = Guid.Parse(courseId)
                 };
 
                 return View(viewModel);
@@ -61,29 +53,17 @@ namespace CourseNet.Web.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create(LectureSelectionFormViewModel viewModel, string courseId)
+        public async Task<IActionResult> Create(LectureSelectionFormViewModel viewModel)
         {
-            bool isInstructor = await instructorService.InstructorExistsByUserId(User.GetId());
-
-            if (!isInstructor)
-            {
-                TempData["ErrorMessage"] = "Вие не сте инструктор! Трябва първо да станете инструктор, за да успеете да създадете лекция към курс";
-                return RedirectToAction("Become", "Instructor");
-            }
-
-            bool lectureExists = await lecturesService.LectureExistsByCourseId(courseId);
-
-            if (lectureExists)
-            {
-                TempData["ErrorMessage"] = "Неуспешно добавяне на лекция към курс!";
-            }
-
             try
             {
-                await lecturesService.CreateLectureAsync(viewModel, courseId);
+                viewModel.CourseId = Guid.Parse(viewModel.CourseId.ToString());
+
+                await lecturesService.CreateLectureAsync(viewModel, viewModel.CourseId.ToString());
             }
             catch (Exception)
             {
+                
                 ModelState.AddModelError(string.Empty, "Възникна грешка при създаването на лекцията!");
                 return RedirectToAction("Details", "Courses");
             }
