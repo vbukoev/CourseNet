1bbbcfb9494402f87efcf6e2f8fe79114c4f57cb
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-03-22 20:31:26 +0200

    delete tasks to the lectureService

--- CourseNet.Services.Data/LectureService.cs
+++ CourseNet.Services.Data/LectureService.cs
@@ -46,5 +46,26 @@ namespace CourseNet.Services.Data
             await context.SaveChangesAsync();
         }
 
+        public async Task<LectureSelectionFormViewModel> GetLectureForDeleteByIdAsync(int lectureId)
+        {
+            var lecture = await context
+                .Lectures
+                .FirstAsync(l => l.Id == lectureId);
+
+            return new LectureSelectionFormViewModel
+            {
+                Title = lecture.Title,
+            };
+        }
+
+        public async Task DeleteLectureByIdAsync(int lectureId)
+        {
+            var lectureToDelete = await context.Lectures.FindAsync(lectureId);
+            if (lectureToDelete != null)
+            {
+                context.Lectures.Remove(lectureToDelete);
+                await context.SaveChangesAsync();
+            }
+        }
     }
 }
