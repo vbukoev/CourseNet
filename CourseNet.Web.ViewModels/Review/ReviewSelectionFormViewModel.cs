8a1a58fd5eaa7568acc8c80e3938d95f43139531
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-03-23 13:33:43 +0200

    Create ReviewSelectionFormViewModel.cs

---
+++ CourseNet.Web.ViewModels/Review/ReviewSelectionFormViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using CourseNet.Data.Models.Entities;
+
+namespace CourseNet.Web.ViewModels.Review
+{
+    public class ReviewSelectionFormViewModel 
+    {
+        [Required(ErrorMessage = "Полето \"{0}\" е задължително.")]
+        [Display(Name = "Оценка за курс")]
+        public int Rating { get; set; }
+
+        [Required(ErrorMessage = "Полето \"{0}\" е задължително.")]
+        [Display(Name = "Коментар")]
+        public string Comment { get; set; }
+
+        [Display(Name = "Дата")]
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; } = DateTime.UtcNow;
+
+        public string CourseId { get; set; }
+
+
+    }
+}
