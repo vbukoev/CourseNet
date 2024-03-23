20133aad70fa142b16f42798dbcd7cab0e67561a
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-03-23 13:33:07 +0200

    Create 20240323111923_ChangedUserToCourseUser.cs

---
+++ CourseNet.Data/Migrations/20240323111923_ChangedUserToCourseUser.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CourseNet.Data.Migrations
+{
+    public partial class ChangedUserToCourseUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_Students_UserId",
+                table: "Reviews");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Students_Courses_CourseId",
+                table: "Students");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Students",
+                table: "Students");
+
+            migrationBuilder.RenameTable(
+                name: "Students",
+                newName: "User");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Students_CourseId",
+                table: "User",
+                newName: "IX_User_CourseId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_User",
+                table: "User",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_User_UserId",
+                table: "Reviews",
+                column: "UserId",
+                principalTable: "User",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_User_Courses_CourseId",
+                table: "User",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_User_UserId",
+                table: "Reviews");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_User_Courses_CourseId",
+                table: "User");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_User",
+                table: "User");
+
+            migrationBuilder.RenameTable(
+                name: "User",
+                newName: "Students");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_User_CourseId",
+                table: "Students",
+                newName: "IX_Students_CourseId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Students",
+                table: "Students",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_Students_UserId",
+                table: "Reviews",
+                column: "UserId",
+                principalTable: "Students",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Students_Courses_CourseId",
+                table: "Students",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "Id");
+        }
+    }
+}
