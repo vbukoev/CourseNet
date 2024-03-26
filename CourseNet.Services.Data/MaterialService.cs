82566326788d89e863c9d5ed6be6e425737ff8cb
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-03-26 17:00:41 +0200

    Update MaterialService.cs

--- CourseNet.Services.Data/MaterialService.cs
+++ CourseNet.Services.Data/MaterialService.cs
@@ -3,10 +3,93 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CourseNet.Data;
+using CourseNet.Data.Models.Entities;
+using CourseNet.Services.Data.Interfaces;
+using CourseNet.Web.ViewModels.Material;
+using Microsoft.EntityFrameworkCore;
 
 namespace CourseNet.Services.Data
 {
-    internal class MaterialService
+    public class MaterialService : IMaterialService
     {
+        private readonly CourseNetDbContext context;
+        private readonly ILectureService lectureService;
+
+        public MaterialService(CourseNetDbContext context, ILectureService lectureService)
+        {
+            this.context = context;
+            this.lectureService = lectureService;
+        }
+        public async Task<IEnumerable<Material>> GetAllMaterialsForLectureAsync(int lectureId)
+        {
+            return await context.Materials
+                .Where(m => m.LectureId == lectureId)
+                .ToListAsync();
+        }
+
+        public async Task AddMaterialToLectureAsync(MaterialSelectionFormViewModel model, int lectureId)
+        {
+            var material = new Material
+            {
+                Name = model.Name,
+                Description = model.Description,
+                LectureId = lectureId,
+            };
+
+            await context.Materials.AddAsync(material);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task<MaterialSelectionFormViewModel> GetMaterialForDeleteByIdAsync(int materialId)
+        {
+            var material = await context.Materials
+                .Where(m => m.Id == materialId)
+                .FirstOrDefaultAsync();
+
+            var materialSelectionFormViewModel = new MaterialSelectionFormViewModel
+            {
+                Name = material.Name,
+                Description = material.Description,
+                LectureId = material.LectureId,
+            };
+
+            return materialSelectionFormViewModel;
+        }
+
+        public async Task DeleteMaterialByIdAsync(int materialId)
+        {
+            var materialToDelete = await context.Materials
+                .Where(m => m.Id == materialId)
+                .FirstAsync();
+
+            context.Materials.Remove(materialToDelete);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task<MaterialSelectionFormViewModel> GetMaterialForUpdateAsync(int materialId)
+        {
+            var material = await context.Materials
+                .Where(m => m.Id == materialId)
+                .FirstAsync();
+
+            return new MaterialSelectionFormViewModel
+            {
+                Name = material.Name,
+                Description = material.Description,
+                LectureId = material.LectureId,
+            };
+        }
+
+        public async Task UpdateMaterialAsync(MaterialSelectionFormViewModel model, int materialId)
+        {
+            var material = await context.Materials
+                .Where(m => m.Id == materialId)
+                .FirstAsync();
+
+            material.Name = model.Name;
+            material.Description = model.Description;
+            material.LectureId = model.LectureId;
+        }
     }
 }
