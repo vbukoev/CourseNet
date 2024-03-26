9a95b961a519126310f63dd6ef94f29fe5b661f4
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-03-26 18:11:00 +0200

    Update CourseNetDbContextModelSnapshot.cs

--- CourseNet.Data/Migrations/CourseNetDbContextModelSnapshot.cs
+++ CourseNet.Data/Migrations/CourseNetDbContextModelSnapshot.cs
@@ -355,36 +355,36 @@ namespace CourseNet.Data.Migrations
                         new
                         {
                             Id = 1,
-                            Description = "Този материал представя основните принципи на обектно-ориентираното програмиране (OOP)(https://bg.wikipedia.org/wiki/%D0%9E%D0%B1%D0%B5%D0%BA%D1%82%D0%BD%D0%BE_%D0%BE%D1%80%D0%B8%D0%B5%D0%BD%D1%82%D0%B8%D1%80%D0%B0%D0%BD%D0%BE_%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%B8%D1%80%D0%B0%D0%BD%D0%B5), включително инкапсулация, наследяване и полиморфизъм(https://bg.wikipedia.org/wiki/%D0%9F%D0%BE%D0%BB%D0%B8%D0%BC%D0%BE%D1%80%D1%84%D0%B8%D0%B7%D1%8A%D0%BC_(%D0%B8%D0%BD%D1%84%D0%BE%D1%80%D0%BC%D0%B0%D1%82%D0%B8%D0%BA%D0%B0)).",
-                            LectureId = 0,
+                            Description = "Този материал представя основните принципи на обектно-ориентираното програмиране (OOP)(https://bg.wikipedia.org/wiki/%D0%9E%D0%B1%D0%B5%D0%BA%D1%82%D0%BD%D0%BE_%D0%BE%D1%80%D0%B8%D0%B5%D0%BD%D1%82%D0%B8%D1%80%D0%B0%D0%BD%D0%BE_%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%B8%D1%80%D0%B0%D0%BD%D0%B5), включително инкапсулация, наследяване и полиморфизъм.",
+                            LectureId = 1,
                             Name = "Принципи на Обектно-ориентираното програмиране"
                         },
                         new
                         {
                             Id = 2,
                             Description = "Този материал представя теми по програмирането на C#(https://bg.wikipedia.org/wiki/C_Sharp),включително работа с LINQ, асинхронно програмиране(https://learn.microsoft.com/bg-bg/dotnet/csharp/asynchronous-programming/) и напреднали структури на данни.",
-                            LectureId = 0,
+                            LectureId = 2,
                             Name = "Програмиране на C#"
                         },
                         new
                         {
                             Id = 3,
                             Description = "Този материал представя основни концепции и технологии в областта на базите данни(https://bg.wikipedia.org/wiki/%D0%91%D0%B0%D0%B7%D0%B0_%D0%B4%D0%B0%D0%BD%D0%BD%D0%B8), включително релационни бази данни, SQL заявки и моделиране на данни.",
-                            LectureId = 0,
+                            LectureId = 3,
                             Name = "Основи на базите данни"
                         },
                         new
                         {
                             Id = 4,
                             Description = "Този материал представя основни концепции в областта на бизнеса(https://bg.wikipedia.org/wiki/%D0%91%D0%B8%D0%B7%D0%BD%D0%B5%D1%81),включително управление на проекти, маркетинг и стратегическо планиране.",
-                            LectureId = 0,
+                            LectureId = 4,
                             Name = "Основи на бизнеса"
                         },
                         new
                         {
                             Id = 5,
                             Description = "Този материал представя основни принципи на дизайна(https://bg.wikipedia.org/wiki/%D0%94%D0%B8%D0%B7%D0%B0%D0%B9%D0%BD), включителноцветове, композиция и типография.",
-                            LectureId = 0,
+                            LectureId = 5,
                             Name = "Дизайн принципи"
                         });
                 });
