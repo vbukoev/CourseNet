bbe8efdab47876f175526b3cc8987d1412432b23
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-03-26 18:10:56 +0200

    Update MaterialEntityConfiguration.cs

--- CourseNet.Data/Configurations/MaterialEntityConfiguration.cs
+++ CourseNet.Data/Configurations/MaterialEntityConfiguration.cs
@@ -29,7 +29,7 @@ namespace CourseNet.Data.Configurations
                 {
                     Id = 1,
                     Name = "Принципи на Обектно-ориентираното програмиране",
-                    Description = "Този материал представя основните принципи на обектно-ориентираното програмиране (OOP)(https://bg.wikipedia.org/wiki/%D0%9E%D0%B1%D0%B5%D0%BA%D1%82%D0%BD%D0%BE_%D0%BE%D1%80%D0%B8%D0%B5%D0%BD%D1%82%D0%B8%D1%80%D0%B0%D0%BD%D0%BE_%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%B8%D1%80%D0%B0%D0%BD%D0%B5), включително инкапсулация, наследяване и полиморфизъм(https://bg.wikipedia.org/wiki/%D0%9F%D0%BE%D0%BB%D0%B8%D0%BC%D0%BE%D1%80%D1%84%D0%B8%D0%B7%D1%8A%D0%BC_(%D0%B8%D0%BD%D1%84%D0%BE%D1%80%D0%BC%D0%B0%D1%82%D0%B8%D0%BA%D0%B0)).",
+                    Description = "Този материал представя основните принципи на обектно-ориентираното програмиране (OOP)(https://bg.wikipedia.org/wiki/%D0%9E%D0%B1%D0%B5%D0%BA%D1%82%D0%BD%D0%BE_%D0%BE%D1%80%D0%B8%D0%B5%D0%BD%D1%82%D0%B8%D1%80%D0%B0%D0%BD%D0%BE_%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%B8%D1%80%D0%B0%D0%BD%D0%B5), включително инкапсулация, наследяване и полиморфизъм.",
                     LectureId = 1,
 
                 },
