6b499db868c0374d92915d1fc075445462efc2c0
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-03-26 17:00:37 +0200

    Update IMaterialService.cs

--- CourseNet.Services.Data/Interfaces/IMaterialService.cs
+++ CourseNet.Services.Data/Interfaces/IMaterialService.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CourseNet.Data.Models.Entities;
+using CourseNet.Web.ViewModels.Material;
 
 namespace CourseNet.Services.Data.Interfaces
 {
-    internal class IMaterialService
+    public interface IMaterialService
     {
+        Task<IEnumerable<Material>> GetAllMaterialsForLectureAsync(int lectureId);
+
+        Task AddMaterialToLectureAsync(MaterialSelectionFormViewModel material, int lectureId);
+
+        public Task<MaterialSelectionFormViewModel> GetMaterialForDeleteByIdAsync(int materialId);
+
+        public Task DeleteMaterialByIdAsync(int materialId);
+
+        public Task<MaterialSelectionFormViewModel> GetMaterialForUpdateAsync(int materialId);
+
+        public Task UpdateMaterialAsync(MaterialSelectionFormViewModel model, int materialId);
     }
 }
