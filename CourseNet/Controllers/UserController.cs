5e4aa8ba34ab915a2d09c42c00de91967388a765
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-03-28 10:22:17 +0200

    Update UserController.cs

--- CourseNet/Controllers/UserController.cs
+++ CourseNet/Controllers/UserController.cs
@@ -1,9 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CourseNet.Data.Models.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CourseNet.Web.Controllers
 {
     public class UserController : Controller
     {
+        private readonly SignInManager<CourseUser> _signInManager;
+        private readonly UserManager<CourseUser> _userManager;
+        private readonly IUserStore<CourseUser> _userStore;
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
         public IActionResult Profile()
         {
             return View();
