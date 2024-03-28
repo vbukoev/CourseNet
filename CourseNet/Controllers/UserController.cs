22d152786ec277d5fb6a825652a2d91d06cb962a
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-03-28 10:43:19 +0200

    Implement Custom Registration

--- CourseNet/Controllers/UserController.cs
+++ CourseNet/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using CourseNet.Data.Models.Entities;
+using CourseNet.Web.ViewModels.User;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,14 +7,60 @@ namespace CourseNet.Web.Controllers
 {
     public class UserController : Controller
     {
-        private readonly SignInManager<CourseUser> _signInManager;
-        private readonly UserManager<CourseUser> _userManager;
-        private readonly IUserStore<CourseUser> _userStore;
+        private readonly SignInManager<CourseUser> signInManager;
+        private readonly UserManager<CourseUser> userManager;
+        private readonly IUserStore<CourseUser> userStore;
+
+        public UserController(
+            UserManager<CourseUser> userManager,
+            IUserStore<CourseUser> userStore,
+            SignInManager<CourseUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.userStore = userStore;
+            this.signInManager = signInManager;
+        }
 
         [HttpGet]
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterFormModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            CourseUser user = new CourseUser()
+            {
+                FirstName = model.FirstName,
+                LastName = model.LastName
+            };
+
+            await userManager.SetEmailAsync(user, model.Email);
+            await userManager.SetUserNameAsync(user, model.Email);
+
+            IdentityResult result =
+                await userManager.CreateAsync(user, model.Password);
+
+            if (!result.Succeeded)
+            {
+                foreach (IdentityError error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+
+                return View(model);
+            }
+
+            await signInManager.SignInAsync(user, false);
+            //memoryCache.Remove(UsersCacheKey);
+
+            return RedirectToAction("Index", "Home");
         }
 
         public IActionResult Profile()
