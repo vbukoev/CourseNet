8b52a3db27517959c6f0efedd1190ba40f26cb62
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-04-02 20:41:47 +0300

    Added summaries to the view Model of Category Details

--- CourseNet.Web.ViewModels/Category/CategoryDetailsViewModel.cs
+++ CourseNet.Web.ViewModels/Category/CategoryDetailsViewModel.cs
@@ -1,12 +1,30 @@
 ﻿using CourseNet.Web.ViewModels.Category.Interfaces;
-using CourseNet.Web.ViewModels.Course;
-
+using System.ComponentModel.DataAnnotations;
+using static CourseNet.Common.DataConstants.GeneralApplicationConstants;
+using static CourseNet.Common.DataConstants.Category;
 namespace CourseNet.Web.ViewModels.Category
 {
+    /// <summary>
+    /// Details View Model for the category
+    /// </summary>
     public class CategoryDetailsViewModel : ICategoryDetailsModel
     {
+        /// <summary>
+        /// Identifier of the category
+        /// </summary>
+        [Required(ErrorMessage = RequireErrorMessage)]
         public int Id { get; set; }
+
+        /// <summary>
+        /// Name of the category
+        /// </summary>
+        [Required(ErrorMessage = RequireErrorMessage)]
+        [StringLength(NameMaxLength, MinimumLength = NameMinLength, ErrorMessage = LengthErrorMessage)]
         public string Name { get; set; } = null!;
+
+        /// <summary>
+        /// Collection of courses in the category
+        /// </summary>
         public IEnumerable<CategorySelectionFormViewModel> Categories { get; set; } = new HashSet<CategorySelectionFormViewModel>();
     }
 }
