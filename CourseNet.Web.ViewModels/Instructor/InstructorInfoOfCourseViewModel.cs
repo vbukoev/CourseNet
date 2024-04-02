78908d6fc0ee50bf605845bc11b09c5ae5aeb3f7
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-04-02 20:46:49 +0300

    Adding summaries to the Instructor info view Model

--- CourseNet.Web.ViewModels/Instructor/InstructorInfoOfCourseViewModel.cs
+++ CourseNet.Web.ViewModels/Instructor/InstructorInfoOfCourseViewModel.cs
@@ -1,12 +1,23 @@
 ﻿using System.ComponentModel.DataAnnotations;
-
+using static CourseNet.Common.DataConstants.GeneralApplicationConstants;
+using static CourseNet.Common.DataConstants.Instructor;
 namespace CourseNet.Web.ViewModels.Instructor
 {
+    /// <summary>
+    /// Instructor Info Of Course View Model
+    /// </summary>
     public class InstructorInfoOfCourseViewModel
     {
-        public string FullName { get; set; } = null!;
-
+        /// <summary>
+        /// Email of the instructor
+        /// </summary>
+        [Required(ErrorMessage = RequireErrorMessage)]
         public string Email { get; set; } = null!;
+        /// <summary>
+        /// Phone Number of the instructor
+        /// </summary>
+        [Required(ErrorMessage = RequireErrorMessage)]
+        [StringLength(PhoneNumberMaxLength, MinimumLength = PhoneNumberMinLength, ErrorMessage = LengthErrorMessage)]
         [Display(Name="Телефон")]
         public string PhoneNumber { get; set; } = null!;
     }
