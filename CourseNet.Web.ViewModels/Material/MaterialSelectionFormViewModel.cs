5850d24ecdd93c55d173bdf946d61273f365a04d
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-04-02 20:48:42 +0300

    Update MaterialSelectionFormViewModel.cs

--- CourseNet.Web.ViewModels/Material/MaterialSelectionFormViewModel.cs
+++ CourseNet.Web.ViewModels/Material/MaterialSelectionFormViewModel.cs
@@ -1,17 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using System.ComponentModel.DataAnnotations;
+using static CourseNet.Common.DataConstants.GeneralApplicationConstants;
+using static CourseNet.Common.DataConstants.Material;
 namespace CourseNet.Web.ViewModels.Material
 {
+    /// <summary>
+    /// Material Selection Form View Model
+    /// </summary>
     public class MaterialSelectionFormViewModel
     {
+        /// <summary>
+        /// Name of the material
+        /// </summary>
+        [Required(ErrorMessage = RequireErrorMessage)]
+        [StringLength(NameMaxLength, MinimumLength = NameMinLength, ErrorMessage = LengthErrorMessage)]
         public string Name { get; set; } = string.Empty;
 
+        /// <summary>
+        /// Description of the material
+        /// </summary>
+        [Required(ErrorMessage = RequireErrorMessage)]
+        [StringLength(DescriptionMaxLength, MinimumLength = DescriptionMinLength, ErrorMessage = LengthErrorMessage)]
         public string Description { get; set; } = string.Empty;
-
+        
+        /// <summary>
+        /// Lecture Identifier
+        /// </summary>
         public int LectureId { get; set; } 
     }
 }
