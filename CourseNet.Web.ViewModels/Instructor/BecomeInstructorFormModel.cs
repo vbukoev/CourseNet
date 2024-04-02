866255c1b0462c2f99ce30f5b96616be428cbd6b
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-04-02 20:42:25 +0300

    Added summaries and anotations to the Become Instructor View Model

--- CourseNet.Web.ViewModels/Instructor/BecomeInstructorFormModel.cs
+++ CourseNet.Web.ViewModels/Instructor/BecomeInstructorFormModel.cs
@@ -1,23 +1,39 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using static CourseNet.Common.DataConstants.GeneralApplicationConstants;
 using static CourseNet.Common.DataConstants.Instructor;
 namespace CourseNet.Web.ViewModels.Instructor
 {
+    /// <summary>
+    /// Instructor Become Form Model
+    /// </summary>
     public class BecomeInstructorFormModel
     {
-        [Required]
-        [StringLength(FirstNameMaxLength, MinimumLength = FirstNameMinLength)]
+        /// <summary>
+        /// Name of the instructor
+        /// </summary>
+        [Required(ErrorMessage = RequireErrorMessage)]
+        [StringLength(FirstNameMaxLength, MinimumLength = FirstNameMinLength, ErrorMessage = LengthErrorMessage)]
         [Display(Name = "Име")]
-        public string FirstName { get; set; }
-        [Required]
-        [StringLength(LastNameMaxLength, MinimumLength = LastNameMinLength)]
-        [Display(Name = "Фамилия")]
-        public string LastName { get; set; }
+        public string FirstName { get; set; } = null!;
 
-        [Required]
-        [StringLength(PhoneNumberMaxLength, MinimumLength = PhoneNumberMinLength)]
-        [Phone]
+        /// <summary>
+        /// Last Name of the instructor
+        /// </summary>
+        [Required(ErrorMessage = RequireErrorMessage)]
+        [StringLength(LastNameMaxLength, MinimumLength = LastNameMinLength, ErrorMessage = LengthErrorMessage)]
+        [Display(Name = "Фамилия")]
+        public string LastName { get; set; } = null!;
+
+        /// <summary>
+        /// PhoneNumber of the instructor
+        /// </summary>
+        [Required(ErrorMessage = RequireErrorMessage)]
+        [StringLength(PhoneNumberMaxLength, MinimumLength = PhoneNumberMinLength, ErrorMessage = LengthErrorMessage)]
         [Display(Name = "Телефонен номер")]
-        public string PhoneNumber{ get; set; }
-        public string Email { get; set; }
+        public string PhoneNumber{ get; set; } = null!;
+        /// <summary>
+        /// Email of the instructor
+        /// </summary>
+        public string Email { get; set; } = null!;
     }
 }
