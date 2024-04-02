d9525fd4e43dd367312963cb59e8f095a0993c1e
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-04-02 20:42:05 +0300

    Added summaries to the view Model of the Category Selection

--- CourseNet.Web.ViewModels/Category/CategorySelectionFormViewModel.cs
+++ CourseNet.Web.ViewModels/Category/CategorySelectionFormViewModel.cs
@@ -1,9 +1,18 @@
 ﻿namespace CourseNet.Web.ViewModels.Category
 {
+    /// <summary>
+    /// Category Selection Form View Model
+    /// </summary>
     public class CategorySelectionFormViewModel
     {
+        /// <summary>
+        /// Category Identifier
+        /// </summary>
         public int Id { get; set; }
 
-        public string Name { get; set; }
+        /// <summary>
+        /// Category Name
+        /// </summary>
+        public string Name { get; set; } = null!;
     }
 }
