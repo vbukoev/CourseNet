49601f84490cc8eb47d5f5b685f494be842a8a58
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-04-03 13:16:05 +0300

    Update IUserService.cs

--- CourseNet.Services.Data/Interfaces/IUserService.cs
+++ CourseNet.Services.Data/Interfaces/IUserService.cs
@@ -1,9 +1,13 @@
-﻿namespace CourseNet.Services.Data.Interfaces
+﻿using CourseNet.Web.ViewModels.User;
+
+namespace CourseNet.Services.Data.Interfaces
 {
     public interface IUserService
     {
         Task<string> GetFullNameByEmailAsync(string email);
 
         Task<string> GetFullNameByIdAsync(string userId);
+
+        Task<IEnumerable<UserViewModel>> GetAllUsersAsync();
     }
 }
