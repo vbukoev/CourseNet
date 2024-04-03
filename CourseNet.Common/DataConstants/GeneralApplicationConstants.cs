964f9e9096ff3787caa44a5e246520ce4318c947
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-04-03 10:52:26 +0300

    Update GeneralApplicationConstants.cs

--- CourseNet.Common/DataConstants/GeneralApplicationConstants.cs
+++ CourseNet.Common/DataConstants/GeneralApplicationConstants.cs
@@ -8,6 +8,7 @@
         public const string RequireErrorMessage = "Полето {0} е задължително за попълване.";
         public const string LengthErrorMessage = "Полето {0} трябва да бъде поне {2} и с максимална дължина {1}.";
 
+        public const string AdminAreaName = "Admin";
         public const string AdministratorRoleName = "Administrator";
         public const string DevelopmentAdminEmail = "admin@coursenet.bg";
     }
