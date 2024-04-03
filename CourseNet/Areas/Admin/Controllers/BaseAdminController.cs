799d2c45a3c52e8d9327f11457df569a21f75735
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-04-03 10:42:37 +0300

    Create BaseAdminController.cs

---
+++ CourseNet/Areas/Admin/Controllers/BaseAdminController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using static CourseNet.Common.DataConstants.GeneralApplicationConstants;
+namespace CourseNet.Web.Areas.Admin.Controllers
+{
+    [Area(AdminAreaName)]
+    [Authorize(Roles = AdministratorRoleName)]
+    public class BaseAdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
