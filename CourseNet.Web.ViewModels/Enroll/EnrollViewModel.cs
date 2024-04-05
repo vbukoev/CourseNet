f57bcdbae7199da3e4e765532c5bb480ca1e4f76
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-04-05 17:48:33 +0300

    Create EnrollViewModel.cs

---
+++ CourseNet.Web.ViewModels/Enroll/EnrollViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace CourseNet.Web.ViewModels.Enroll
+{
+    public class EnrollViewModel
+    {
+        public string Title { get; set; } = null!;
+
+        public string ImageUrl { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+
+        public string InstructorFullName { get; set; } = null!;
+
+        public string InstructorEmail { get; set; } = null!;
+
+        public string StudentFullName { get; set; } = null!;
+
+        public string StudentEmail { get; set; } = null!;
+    }
+}
