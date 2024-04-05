80ee24720b4608af97f8eb594bc5bf8453d73b41
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-04-05 17:48:35 +0300

    Create EnrollController.cs

---
+++ CourseNet/Areas/Admin/Controllers/EnrollController.cs
@@ -0,0 +1,22 @@
+﻿using CourseNet.Services.Data.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseNet.Web.Areas.Admin.Controllers
+{
+    public class EnrollController : BaseAdminController
+    {
+        private readonly IEnrollService enrollService;
+
+        public EnrollController(IEnrollService enrollService)
+        {
+            this.enrollService = enrollService;
+        }
+
+        [Route("Enroll/All")]
+        public async Task<IActionResult> All()
+        {
+            var allEnrollments = await enrollService.GetAllAsync();
+            return View(allEnrollments);
+        }
+    }
+}
