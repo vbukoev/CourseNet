2edc1cb698103afc813f9502f67712ba5d137c68
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-04-06 17:43:43 +0300

    Update InstructorServiceTests.cs

--- CourseNet.Services.Tests/InstructorServiceTests.cs
+++ CourseNet.Services.Tests/InstructorServiceTests.cs
@@ -1,8 +1,8 @@
 using CourseNet.Data;
+using CourseNet.Data.Models.Entities;
 using CourseNet.Services.Data;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Infrastructure;
-
+using static CourseNet.Services.Tests.DatabaseSeeder;
 namespace CourseNet.Services.Tests
 {
     public class InstructorServiceTests
@@ -14,9 +14,9 @@ namespace CourseNet.Services.Tests
         public void OneTimeSetup()
         {
             dbOptions = new DbContextOptionsBuilder<CourseNetDbContext>()
-                .UseInMemoryDatabase("CourseNetDB2024" + Guid.NewGuid().ToString())
+                .UseInMemoryDatabase("CourseNetInMemory" + Guid.NewGuid().ToString())
                 .Options;
-            context = new CourseNetDbContext(dbOptions, false);
+            context = new CourseNetDbContext(dbOptions);
 
             context.Database.EnsureCreated();
             SeedDatabase(context);
@@ -25,9 +25,63 @@ namespace CourseNet.Services.Tests
         }
 
         [Test]
-        public void Test1()
+        public async Task InstructorExistsByUserIdAsyncShouldReturnTrueWhenExists()
         {
-            Assert.Pass();
+            string existingInstructorUserId = InstructorUser.Id.ToString();
+
+            bool result = await instructorService.InstructorExistsByUserId(existingInstructorUserId);
+
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public async Task InstructorExistsByUserIdAsyncShouldReturnFalseWhenNotExists()
+        {
+            string nonExistingInstructorUserId = StudentUser.Id.ToString();
+
+            bool result = await instructorService.InstructorExistsByUserId(nonExistingInstructorUserId);
+
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public async Task InstructorExistsByPhoneNumberAsyncShouldReturnTrueWhenExists()
+        {
+            string existingInstructorPhoneNumber = "+359123456789";
+
+            bool result = await instructorService.InstructorExistsByPhoneNumber(existingInstructorPhoneNumber);
+
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public async Task InstructorExistsByPhoneNumberAsyncShouldReturnFalseWhenNotExists()
+        {
+            string nonExistingInstructorPhoneNumber = "+359987654321";
+
+            bool result = await instructorService.InstructorExistsByPhoneNumber(nonExistingInstructorPhoneNumber);
+
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public async Task HasAppliedCoursesByUserIdAsyncShouldReturnTrueWhenHasAppliedCourses()
+        {
+            string existingInstructorUserId = "120206E2-D1E9-4B04-BF2C-943FE9A1793D";
+
+            bool result = await instructorService.HasAppliedCoursesByUserIdAsync(existingInstructorUserId);
+
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public async Task HasAppliedCoursesByUserIdAsyncShouldReturnFalseWhenHasNotAppliedCourses()
+        {
+            string nonExistingInstructorUserId = StudentUser.Id.ToString();
+
+            bool result = await instructorService.HasAppliedCoursesByUserIdAsync(nonExistingInstructorUserId);
+
+            Assert.IsFalse(result);
         }
     }
 }
