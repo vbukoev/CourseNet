4f1896dda088e1a59e72b19d817a95ce3d958053
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-04-10 11:11:23 +0300

    Update CategoryServiceTests.cs

--- CourseNet.Services.Tests/CategoryServiceTests.cs
+++ CourseNet.Services.Tests/CategoryServiceTests.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using CourseNet.Data;
+using CourseNet.Services.Data;
+using CourseNet.Services.Data.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using static CourseNet.Services.Tests.DatabaseSeeder;
 namespace CourseNet.Services.Tests
 {
-    internal class CategoryServiceTests
+    public class CategoryServiceTests
     {
+        private DbContextOptions<CourseNetDbContext> dbOptions;
+        private CourseNetDbContext context;
+        private ICategoryService categoryService;
+
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+            dbOptions = new DbContextOptionsBuilder<CourseNetDbContext>()
+                .UseInMemoryDatabase("CourseNetInMemory" + Guid.NewGuid().ToString())
+                .Options;
+            context = new CourseNetDbContext(dbOptions);
+
+            context.Database.EnsureCreated();
+            SeedDatabase(context);
+
+            categoryService = new CategoryService(context);
+        }
     }
 }
