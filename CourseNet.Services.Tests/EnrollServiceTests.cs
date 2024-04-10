4059d7e097a35e84fdb0a353ba9571b87a866444
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-04-10 11:08:32 +0300

    Create EnrollServiceTests.cs

---
+++ CourseNet.Services.Tests/EnrollServiceTests.cs
@@ -0,0 +1,37 @@
+﻿using CourseNet.Data;
+using CourseNet.Services.Data;
+using CourseNet.Services.Data.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+using static CourseNet.Services.Tests.DatabaseSeeder;
+namespace CourseNet.Services.Tests
+{
+    public class EnrollServiceTests
+    {
+        private DbContextOptions<CourseNetDbContext> dbOptions;
+        private CourseNetDbContext context;
+        private IEnrollService enrollService;
+
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+            dbOptions = new DbContextOptionsBuilder<CourseNetDbContext>()
+                .UseInMemoryDatabase("CourseNetInMemory" + Guid.NewGuid().ToString())
+                .Options;
+            context = new CourseNetDbContext(dbOptions);
+
+            context.Database.EnsureCreated();
+            SeedDatabase(context);
+
+            enrollService = new EnrollService(context);
+        }
+
+        [Test]
+        public async Task GetAllAsyncShouldReturnAllEnrollments()
+        {
+            var enrollments = await enrollService.GetAllAsync();
+
+            Assert.AreEqual(0, enrollments.Count());
+        }
+    }
+}
