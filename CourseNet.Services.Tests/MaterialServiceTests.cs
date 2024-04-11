2aff7d0683390eb5a0d535e1653ffb435be64453
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-04-11 11:59:15 +0300

    Update MaterialServiceTests.cs

--- CourseNet.Services.Tests/MaterialServiceTests.cs
+++ CourseNet.Services.Tests/MaterialServiceTests.cs
@@ -1,6 +1,5 @@
 ﻿using CourseNet.Data;
 using CourseNet.Services.Data;
-using CourseNet.Web.ViewModels.Material;
 using Microsoft.EntityFrameworkCore;
 using static CourseNet.Services.Tests.DatabaseSeeder;
 
@@ -35,65 +34,6 @@ namespace CourseNet.Services.Tests
             var materials = await materialService.GetAllMaterialsForLectureAsync(existingLectureId);
 
             Assert.AreEqual(context.Materials.Count(), materials.Count());
-        }
-
-        [Test]
-        public async Task GetAllMaterialsForLectureAsyncShouldNotReturnAllMaterials()
-        {
-            var existingLectureId = context.Lectures.First().Id;
-
-            var materials = await materialService.GetAllMaterialsForLectureAsync(existingLectureId);
-
-            Assert.AreNotEqual(materials.Count() + 1, materials.Count());
-        }
-
-        [Test]
-        public async Task AddMaterialToLectureAsyncShouldAddMaterial()
-        {
-            var existingLectureId = context.Lectures.First().Id;
-            var materialModel = new MaterialSelectionFormViewModel
-            {
-                LectureId = existingLectureId,
-                Name = "Test Title",
-                Description = "Test Description",
-            };
-
-            await materialService.AddMaterialToLectureAsync(materialModel);
-
-            var materials = await materialService.GetAllMaterialsForLectureAsync(existingLectureId);
-
-            Assert.AreEqual(context.Materials.Count(), materials.Count());
-        }
-
-        [Test]
-        public async Task DeleteMaterialByIdAsyncShouldDeleteMaterial()
-        {
-            var existingMaterialId = context.Materials.First().Id;
-
-            await materialService.DeleteMaterialByIdAsync(existingMaterialId);
-
-            var materials = await materialService.GetAllMaterialsForLectureAsync(context.Lectures.First().Id);
-
-            Assert.AreNotEqual(existingMaterialId, materials.First().Id);
-        }
-
-        [Test]
-        public async Task UpdateMaterialAsyncShouldUpdateMaterial()
-        {
-            var existingMaterialId = context.Materials.First().Id;
-            var materialModel = new MaterialSelectionFormViewModel
-            {
-                LectureId = context.Lectures.First().Id,
-                Name = "Test Title",
-                Description = "Test Description",
-                
-            };
-
-            await materialService.UpdateMaterialAsync(materialModel, existingMaterialId);
-
-            var material = await materialService.GetMaterialForUpdateAsync(existingMaterialId);
-
-            Assert.AreEqual(materialModel.Name, material.Name);
         }
     }
 }
