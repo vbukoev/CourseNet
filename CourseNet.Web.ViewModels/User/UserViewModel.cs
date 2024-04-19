8f352cb4dee20948bbca9bd8b8a01c12609e718d
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-04-19 16:37:31 +0300

    Update UserViewModel.cs

--- CourseNet.Web.ViewModels/User/UserViewModel.cs
+++ CourseNet.Web.ViewModels/User/UserViewModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CourseNet.Web.ViewModels.User
+﻿namespace CourseNet.Web.ViewModels.User
 {
     public class UserViewModel
     {
